75f5731543ee12f67ee2301ba98b8b8920f02930
heyuuuu <heyu1995@live.com>
2018-04-16 23:01:05 +0800

    studyASMDesmo/target/test-classes/com/heyu/studyASMDesmo/studyASMDesmo/He# 1@312iiiiiii3123!@3123123213t staged for commit:

--- studyASMDesmo/src/test/java/com/heyu/studyASMDesmo/studyASMDesmo/DemoTest.java
+++ studyASMDesmo/src/test/java/com/heyu/studyASMDesmo/studyASMDesmo/DemoTest.java
@@ -1,12 +1,41 @@
 package com.heyu.studyASMDesmo.studyASMDesmo;
 
+import jdk.internal.org.objectweb.asm.*;
 import org.junit.Test;
 
-public class DemoTest {
-	
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+public class DemoTest extends ClassLoader{
+
 	@Test
-	public void demoTest(){
-		System.out.println("111");
+	public void demoTest() throws IOException,InstantiationException,IllegalAccessException,InvocationTargetException{
+		ClassReader cr = new ClassReader(HelloWorld.class.getName());
+		ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+		CustomVistor cv = new CustomVistor(Opcodes.ASM4,cw);
+		cr.accept(cv,0);
+		byte[] code = cw.toByteArray();
+		DemoTest loader = new DemoTest();
+		Class clazz = loader.defineClass(null, code, 0, code.length);
+		clazz.getMethods()[0].invoke(clazz.newInstance(),new Object[]{});
 	}
 
+
 }
+class CustomVistor extends ClassVisitor implements Opcodes{
+	public CustomVistor(int api, ClassVisitor cv){
+		super(api, cv);
+	}
+
+	@Override
+	public MethodVisitor visitMethod(int i, String s, String s1, String s2, String[] strings) {
+		MethodVisitor mv = super.visitMethod(i,s,s1,s2,strings);
+		if(s.equals("sayHello")){
+			mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+			mv.visitMethodInsn(INVOKESTATIC, "java/lang/System", "currentTimeMillis", "()J");
+			mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(J)V");
+		}
+		return mv;
+	}
+}
+
